3438394d22ae8ccc6238377d31224c03e1f8451a
Ryan Norman <rnorman@covermymeds.com>
2021-12-03 21:44:05 -0500

    Asserts increase and decrease counted correctly on second depth measurement

--- measure-depth.rb
+++ measure-depth.rb
@@ -1,10 +1,33 @@
 class DepthMeasurer
   def initialize(depths = nil)
     @depths = depths || File.read('./input/sub-depths.txt')
+    @previous_depth = nil
+    @counter = 0
   end
 
   def measure
-    'N/A - no previous measurement'
+    msg = nil
+    if @previous_depth.nil?
+      msg = 'N/A - no previous measurement'
+    elsif current_depth < @previous_depth
+      msg = 'decreased'
+    elsif current_depth > @previous_depth
+      msg = 'increased'
+    elsif current_depth == @previous_depth
+      msg = 'no change'
+    end
+
+    @previous_depth = current_depth
+    @counter += 1
+    msg
+  end
+
+  def current_depth
+    depths[@counter]
+  end
+
+  def depths
+    @depths.split("\n").collect(&:to_i)
   end
 end
 
